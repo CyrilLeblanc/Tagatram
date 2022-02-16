0f3e0f1c2412c07b7256813df10590d88557c94e
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-02-16 15:48:48 +0100

    set dark theme in cache and update map

--- src/app/services/theme-changer.service.ts
+++ src/app/services/theme-changer.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { Storage } from '@ionic/storage';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeChangerService {
-
   private theme: string = 'dark';
 
-  constructor() { }
+  constructor(private storage: Storage) {
+    this.storage.create();
+    this.storage.get('theme').then((val) => {
+      this.setTheme(val);
+    });
+  }
 
   setTheme(temp: string) {
     this.theme = temp;
+    this.storage.set('theme', temp);
   }
 
   getTheme(): string {
     return this.theme;
   }
-
 }
